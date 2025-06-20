ca3d18eaa4d48ecb9e405738e6711add7a451b65
CelineLesur <141427131+CelineLesur@users.noreply.github.com>
2025-06-20 21:02:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
 import uvicorn
-import time
 from gradio_interface import app
 
 if __name__ == "__main__":
@@ -10,7 +9,3 @@ if __name__ == "__main__":
         uvicorn.run(app, host="0.0.0.0", port=port)
     except Exception as e:
         print("Erreur au lancement de Uvicorn :", e)
-    finally:
-        print("Processus terminé ?")
-        while True:
-            time.sleep(10)
